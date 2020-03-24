892fbe4dd1b09b425f62bea75219a61ee2f00f2c
Daniel <hesseldaniel21@gmail.com>
2020-03-24 19:36:36 -0300

    backend: preaparaçao do CORS realizada

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+//Determinado quem poder'a acessar a aplicaçao
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
 
+app.use(cors(
+    // origin: ''
+))
 app.use(express.json())
 app.use(routes)
 
