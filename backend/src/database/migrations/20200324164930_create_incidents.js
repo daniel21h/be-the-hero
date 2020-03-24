d5571afd17d08714751ed3ba274b328f58f92a81
Daniel <hesseldaniel21@gmail.com>
2020-03-24 16:58:30 -0300

    backend: chave estrangeira para ID da app. criada

--- backend/src/database/migrations/20200324164930_create_incidents.js
+++ backend/src/database/migrations/20200324164930_create_incidents.js
@@ -10,9 +10,10 @@ exports.up = function(knex) {
         table.string('ong_id').notNullable()
 
         // criando a chave estrangeira para o id
+        table.foreign('ong_id').references('id').inTable('ongs')
     })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('incidents')
 };
