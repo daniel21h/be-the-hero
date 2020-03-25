0f0deb00c14a32ebbed2230c629381e39bf42962
Daniel <hesseldaniel21@gmail.com>
2020-03-25 17:01:22 -0300

    frontend: design da pagina de Login finalizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css'
+
+import Logon from './pages/Logon'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bem vindo a Semana Omnistack!</p>
-        <p>
-          Opa, tudo bom? O que acha de ver o trabalho do Daniel Hessel no primeiro dia da semana Omnistack 11?
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/daniel21h/be-the-hero"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Node.js
-        </a>
-      </header>
-    </div>
+    <Logon />
   );
 }
 
