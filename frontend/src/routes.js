4f4c3a2487c6106b1302ad081c8f07ed48e6ff88
Daniel <hesseldaniel21@gmail.com>
2020-03-25 17:18:48 -0300

    frontend: rotas de login e registro criadas

---
+++ frontend/src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+//Switch permite que apenas uma rota seja chamada por momento, mesmo que o caminho delas for semelhante
+
+import Logon from './pages/Logon'
+import Register from './pages/Register'
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Logon} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
