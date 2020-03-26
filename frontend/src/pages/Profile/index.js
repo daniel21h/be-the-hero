1ec563227f94bb658ae35bc541d5147b6c1026c2
Daniel <hesseldaniel21@gmail.com>
2020-03-26 10:05:18 -0300

    frontend: tela de cadastro

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FiPower } from 'react-icons/fi'
+import { FiPower, FiTrash2 } from 'react-icons/fi'
 
+import './styles.css'
 import logoImg from '../../assets/logo.svg'
 
 export default function Profile() {
@@ -16,6 +17,67 @@ export default function Profile() {
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
+
+            <h1>Casos cadastrados</h1>
+
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇAO:</strong>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, ea! Assumenda accusamus aspernatur est? In, architecto. Deserunt odio ipsum, necessitatibus neque soluta unde possimus, exercitationem voluptatum vero sequi vitae ad!</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇAO:</strong>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, ea! Assumenda accusamus aspernatur est? In, architecto. Deserunt odio ipsum, necessitatibus neque soluta unde possimus, exercitationem voluptatum vero sequi vitae ad!</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇAO:</strong>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, ea! Assumenda accusamus aspernatur est? In, architecto. Deserunt odio ipsum, necessitatibus neque soluta unde possimus, exercitationem voluptatum vero sequi vitae ad!</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇAO:</strong>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, ea! Assumenda accusamus aspernatur est? In, architecto. Deserunt odio ipsum, necessitatibus neque soluta unde possimus, exercitationem voluptatum vero sequi vitae ad!</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+            </ul>
         </div>
     )
 }
