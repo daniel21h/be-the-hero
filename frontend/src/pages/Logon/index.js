2a922f043b0659e468115bf4d35bccc721f54208
Daniel <hesseldaniel21@gmail.com>
2020-03-26 12:06:07 -0300

    frontend: persistencia de dados de login e autenticaçao

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,22 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
 
+import api from '../../services/api'
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 
 export default function Logon() {
+    const [id, setId] = useState('')
+    const history = useHistory()
+
+    //Persistindo Login e validando exixtencia da ONG
+    async function handleLogin(e) {
+        //Chamando o (e) na function e evitando o compaortamento padrao de recarregamento da pagina // ao realizar o cadastro
+        e.preventDefault()
+
+        //Aguardando o envio do ID da ONG
+        try {
+            const response = await api.post('sessions', { id })
+
+            //Eh preciso ter isso disponivel em todo a aplica~ao(persistir login)
+            localStorage.setItem('ongId', id)
+            localStorage.setItem('ongName', response.data.name)
+
+            history.push('/profile')
+
+        } catch (err) {
+            alert('Falha no login, tente novamente.')
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero"/>
 
-                <form action="">
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID" />
+                    <input 
+                        placeholder="Sua ID" 
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
+
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,11 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import logoImg from '../../assets/logo.svg'
 import './styles.css'
 
+
 export default function Register() {
+
+    // Armazenando os dados recebidos nos input's e enviando para a API
+    //name=valor e setName=atualiza o valor
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [whatsapp, setWhatsapp] = useState('')
+    const [city, setCity] = useState('')
+    const [uf, setUf] = useState('')
+
+    //Enviando o usuario ed volta a page de login apos o cadastro
+    const history = useHistory()
+
+    async function handleRegister(e) {
+        //Chamando o (e) na function e evitando o compaortamento padrao de recarregamento da pagina // ao realizar o cadastro
+        e.preventDefault()
+
+        const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        }
+
+        //Enviando dados para API
+        try {
+            const response = await api.post('ongs', data)
+            //Se der certo
+            alert(`Seu ID de acesso: ${response.data.id}`)
+
+            //Enviando o usuario ed volta a page de login apos o cadastro
+            history.push('/')
+            //Se falhar
+        } catch (err) {
+            alert('Erro no cadastro, tente novamente.')
+        }
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -20,14 +61,40 @@ export default function Register() {
                         Nao tenho cadastro
                     </Link>
                 </section>
-                <form action="">
-                    <input placeholder="Nome da ONG" />
-                    <input type="email"placeholder="E-mail" />
-                    <input placeholder="WhatsApp" />
+                <form onSubmit={handleRegister}>
+                    <input 
+                        placeholder="Nome da ONG" 
+                        //props. que estao abaixo only devem ser definidas apos a conexao com API
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+
+                    <input 
+                        type="email"
+                        placeholder="E-mail" 
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <input 
+                        placeholder="WhatsApp" 
+                        value={whatsapp}
+                        onChange={e => setWhatsapp(e.target.value)}
+                    />
 
                     <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style={{ width: 80 }} />
+                        <input 
+                            placeholder="Cidade" 
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                        />
+
+                        <input 
+                            placeholder="UF" 
+                            style={{ width: 80 }} 
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                        />
                     </div>
 
                     <button className="button" type="submit">Cadastrar</button>
